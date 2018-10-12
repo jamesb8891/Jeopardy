a95483075471670fd3644690be04ce92b56d6215
Loryn Mason <masonloryn@gmail.com>
2018-10-12 12:19:30 -0600

    Rename js files, add domUpdates file

---
+++ jsFiles/classes.js
@@ -0,0 +1,50 @@
+class Game {
+  constructor(gameState, round, players) {
+    this.gameState = gameState;
+    this.round = round;
+    this.players = players;
+    // (array of instances of players)
+  }
+  // start()
+  // reset()
+  // pause()
+  // advanceRound()
+  // declareWinner()
+}
+
+class Clues {
+  constructor(question, answer, pointValue) {
+    this.question = question;
+    this.answer = answer;
+    this.pointValue = pointValue;
+    this.categoryId = categoryId;
+  }
+}
+
+class DailyDouble extends Clues {
+  constructor(question, answer, pointValue) {
+    super(question, answer, pointValue);
+  }
+  // checkWager()
+}
+
+class GameBoard {
+  constructor(askedQuestions, unaskedQuestions, categories) {
+    this.askedQuestions = askedQuestions;
+    this.unaskedQuestions = unaskedQuestions;
+    this.categories = categories;
+  }
+}
+
+class Player {
+  constructor(score, id) {
+    this.score = score;
+    this.id = id;
+    // name
+  }
+  // submitAnswer()
+  // submitWager()
+  // updateScore()
+}
+
+module.exports = { Game, Clues, DailyDouble, GameBoard, Player };
--- jsFiles/main.js
+++ jsFiles/main.js
@@ -1,50 +1,48 @@
-class Game {
-  constructor(gameState, round, players) {
-    this.gameState = gameState;
-    this.round = round;
-    this.players = players;
-    // (array of instances of players)
-  }
-  // start()
-  // reset()
-  // pause()
-  // advanceRound()
-  // declareWinner()
+
+var playerOneInput = document.querySelector('.player-one-input');
+var playerTwoInput = document.querySelector('.player-two-input');
+var playerThreeInput = document.querySelector('.player-three-input');
+var playerOneName = document.querySelector('.player-one-name');
+var playerTwoName = document.querySelector('.player-two-name');
+var playerThreeName = document.querySelector('.player-three-name');
+var startButton = document.querySelector('.start-button');
+var startMenu = document.querySelector('.start-menu');
+var category1 = document.querySelector('.category1');
+var category2 = document.querySelector('.category2');
+var category3 = document.querySelector('.category3');
+var category4 = document.querySelector('.category4');
+let categoriesArray = []
+
+
+startButton.addEventListener("click", initializePlayers);
+
+function initializePlayers() {
+  playerOneName.innerText = playerOneInput.value || "Player 1";
+  playerTwoName.innerText = playerTwoInput.value || "Player 2";
+  playerThreeName.innerText = playerThreeInput.value || "Player 3";
+  hideMenu();
 }
 
-class Clues {
-  constructor(question, answer, pointValue) {
-    this.question = question;
-    this.answer = answer;
-    this.pointValue = pointValue;
-    this.categoryId = categoryId;
-  }
+const hideMenu = () => {
+  startMenu.remove()
 }
 
-class DailyDouble extends Clues {
-  constructor(question, answer, pointValue) {
-    super(question, answer, pointValue);
-  }
-  // checkWager()
+const domCategories = () => {
+  category1.innerText = categoriesArray[0]
+  category2.innerText = categoriesArray[1]
+  category3.innerText = categoriesArray[2]
+  category4.innerText = categoriesArray[3]
 }
 
-class GameBoard {
-  constructor(askedQuestions, unaskedQuestions, categories) {
-    this.askedQuestions = askedQuestions;
-    this.unaskedQuestions = unaskedQuestions;
-    this.categories = categories;
+const setCategories= () => {
+  for(var i = 0; categoriesArray.length < 4; i++) {
+    let newCat = (Object.keys(data.categories)[Math.floor(Math.random()* (0, 10))])
+    if (!categoriesArray.includes(newCat)) {
+    categoriesArray.push(newCat);
+    }
   }
+  domCategories()
+  return categoriesArray
 }
+setCategories()
 
-class Player {
-  constructor(score, id) {
-    this.score = score;
-    this.id = id;
-    // name
-  }
-  // submitAnswer()
-  // submitWager()
-  // updateScore()
-}
-
-module.exports = { Game, Clues, DailyDouble, GameBoard, Player };
