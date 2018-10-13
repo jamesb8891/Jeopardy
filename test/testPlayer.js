558bfaae8b40583eea2716152d5dcedd8e23c01a
Loryn Mason <masonloryn@gmail.com>
2018-10-13 15:54:21 -0600

    Add test for classes

---
+++ test/testPlayer.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Player = require('../jsFiles/classes/playerClass.js');
+
+describe('Player', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  })
+
+  it('should have a default score of 0', function() {
+    let player1 = new Player();
+    expect(player1.score).to.equal(0);
+  })
+
+  it('should be able to have an id', function() {
+    let player1 = new Player('player1');
+    expect(player1.id).to.equal('player1');
+  })
+})
