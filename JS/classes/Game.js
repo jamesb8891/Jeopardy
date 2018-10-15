386a62e5ef84ec599ca92d660003b4b35cf8fe40
James Burbage <jeburbage@gmail.com>
2018-10-15 12:06:46 -0600

    Edit file names

--- JS/classes/Game.js
+++ JS/classes/Game.js
@@ -2,20 +2,18 @@ class Game {
   constructor(gameState) {
     this.gameState = gameState;
     this.round = 1;
-    this.players = ['player1', 'player2', 'player3'];
+    this.players = ["player1", "player2", "player3"];
   }
-  
+
   advanceRound() {
-    return this.round++
+    return this.round++;
   }
   // declareWinner()
   // start()
   // reset()
   // advanceRound()
-
 }
 
-
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = Game;
 }
